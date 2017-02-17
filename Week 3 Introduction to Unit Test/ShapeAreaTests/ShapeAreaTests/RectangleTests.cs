40eebe16148ab3d4bf865e77206e996547208b50
Nicholas Wan <Nicholas Wan>
2017-02-17 00:30:19 +0000

    Added Unit Tests for Shape Tests

---
+++ Week 3 Introduction to Unit Test/ShapeAreaTests/ShapeAreaTests/RectangleTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShapeAreaTests
+{
+    [TestClass]
+    public class RectangleTests
+    {
+        [TestMethod] // Testing the area of a rectangle
+        public void Should_ReturnAreaOf20WhenLengthIs10AndWidthIs2()
+        {
+            // Arrange
+            var rectangle = new Rectangle(10, 2);
+            // Act
+            var result = rectangle.getArea();
+            // Assert
+            Assert.AreEqual(result, 20);
+        }
+
+        [TestMethod]
+        public void Should_ReturnAreaof25WhenLengthIs5AndWidthIs5()
+        {
+            // Arrange
+            var rectangle = new Rectangle(5, 5);
+
+            // Act
+            var result = rectangle.getArea();
+            
+            // Assert
+            Assert.AreEqual(result, 25);
+        }
+
+        [TestMethod] // Testing the perimeter of a rectangle
+        public void Should_ReturnPerimeterof40WhenLengthIs10AndWidthIs10()
+        {
+            // Arrange
+            var rectangle = new Rectangle(10, 10);
+
+            // Act
+            var result = rectangle.getPerimeter();
+
+            // Assert
+            Assert.AreEqual(result, 40);
+        }
+    }
+
+    class Rectangle
+    {
+        // Can only be accessed directly through the class / constructor
+        private int _length;
+        private int _width;
+
+        public Rectangle(int length, int width)
+        {
+            _length = length;
+            _width = width;
+        }
+
+        // It does not return a value void
+
+        public int getArea()
+        {
+            return _length * _width;
+        }
+
+        public int getPerimeter()
+        {
+            return _length + _length + _width + _width;
+        }
+    }
+}
---
+++ Week 3 Introduction to Unit Test/ShapeTests/ShapeTests/CircleTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShapeTests
+{
+    [TestClass]
+    public class CircleTests
+    {
+        [TestMethod] // Calculating the area of a circle
+        public void Should_ReturnAreaOfOneHundredAndThirteenPointFour()
+        {
+            // Arrange
+            var circle = new Circle(6, 0);
+            // Act
+            var result = circle.getArea();
+            // Assert
+            Assert.AreEqual(result, 113);
+        }
+
+        [TestMethod] // C = 3.14 X Diameter
+        public void Should_ReturnCircumferenceOfThreeHundredAndForteen()
+        {
+            // Arrange
+            var circle = new Circle(0, 100);
+            // Act
+            var result = circle.getCircumference();
+            // Assert
+            Assert.AreEqual(result, 314);
+        }
+    }
+
+    class Circle
+    {
+        private int _radius;
+        //double pi = Math.PI;
+        double pi = 3.14;
+        private int _diameter;
+
+        
+
+        public Circle(int radius1, int diameter)
+        {
+            _radius = radius1;
+            _diameter = diameter;
+            
+        }
+
+        public int getArea()
+        {
+            var result =  _radius * _radius * pi;
+            return (int)result;
+        }
+
+         public double getCircumference()
+        {
+            return _diameter * pi;
+        }
+    }
+}
